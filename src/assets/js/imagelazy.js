683762895ebf6c6fb1431995fe3ca2f4c83efc87
falstack <icesilt@outlook.com>
2018-06-28 21:03:24 +0800

    update: image

--- src/assets/js/imagelazy.js
+++ src/assets/js/imagelazy.js
@@ -32,19 +32,56 @@ export default {
         return ['scroll']
       }
     },
-    id: {
-      type: [Number, String],
-      default: 0
-    },
+    id: {},
     aspect: {
       type: Number,
       default: 0
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    source: {
+      type: Object,
+      default: null
+    },
+    full: {
+      type: Boolean,
+      default: false
     }
   },
-  render: function (createElement) {
-    return createElement(this.tag, {
+  render: function (h) {
+    if (!this.canRender) {
+      return h(this.tag, {
+        style: this.aspect ? {
+          paddingBottom: `${this.aspect * 100}%`
+        } : {}
+      })
+    }
+    const containSize = this.full && this.source && this.source.width < window.innerWidth
+    if (containSize) {
+      return h('div',
+        [
+          h(this.tag, {
+            'class': [
+              'image-shim',
+              {
+                'image-lazy-init': this.aspect,
+                'image-loading': this.loading
+              }
+            ],
+            style: {
+              width: `${this.source.width}px`,
+              height: `${this.source.height}px`
+            }
+          })
+        ]
+      )
+    }
+    return h(this.tag, {
       'class': {
-        'image-lazy-init': this.aspect
+        'image-lazy-init': this.aspect,
+        'image-loading': this.loading
       },
       style: this.aspect ? {
         paddingBottom: `${this.aspect * 100}%`
@@ -54,34 +91,33 @@ export default {
   data () {
     return {
       listeners: {},
-      resource: this.src
+      resource: this.src,
+      canRender: false
     }
   },
   mounted () {
-    this.init()
+    this.canRender = true
+    this.$nextTick(() => {
+      if (this.$checkInView(this.$el, (this.scale - 0))) {
+        this.loadResource(this.$el)
+      } else {
+        const id = this.$eventManager.add(document.querySelector('._viewport'), this.events, this.$utils.throttle(() => {
+          if (this.$checkInView(this.$el, (this.scale - 0))) {
+            this.loadResource(this.$el)
+            this.$eventManager.del(id)
+          }
+        }, 200))
+        if (this.id) {
+          this.$channel.$on(`image-load-${this.id}`, () => {
+            this.loadResource(this.$el)
+            this.$eventManager.del(id)
+            this.$channel.$off(`image-load-${this.id}`)
+          })
+        }
+      }
+    })
   },
   methods: {
-    init () {
-      this.$nextTick(() => {
-        if (this.$checkInView(this.$el, (this.scale - 0))) {
-          this.loadResource(this.$el)
-        } else {
-          const id = this.$eventManager.add(document, this.events, this.$utils.throttle(() => {
-            if (this.$checkInView(this.$el, (this.scale - 0))) {
-              this.loadResource(this.$el)
-              this.$eventManager.del(id)
-            }
-          }, 200))
-          if (this.id) {
-            this.$channel.$on(`image-load-${this.id}`, () => {
-              this.loadResource(this.$el)
-              this.$eventManager.del(id)
-              this.$channel.$off(`image-load-${this.id}`)
-            })
-          }
-        }
-      })
-    },
     loadResource (image) {
       let src
       if (this.width && this.height) {
@@ -104,19 +140,27 @@ export default {
         src = this.$resize(this.resource)
       }
 
-      if (this.tag.toLowerCase() === 'img') {
-        image.setAttribute('src', src)
-        if (this.aspect) {
-          image.addEventListener('load', () => {
-            image.removeAttribute('style')
-            image.classList.add('image-lazy-active')
-            setTimeout(() => {
-              image.classList.remove('image-lazy-init', 'image-lazy-active')
-            }, 300)
+      const isImage = this.tag.toLowerCase() === 'img'
+      const containSize = this.full && this.source && this.source.width < window.innerWidth
+      const imageTag = containSize ? image.querySelector('.image-shim') : image
+      const useFade = this.aspect
+
+      if (isImage) {
+        imageTag.setAttribute('src', src)
+        if (useFade || this.loading) {
+          imageTag.addEventListener('load', () => {
+            imageTag.removeAttribute('style')
+            imageTag.classList.remove('image-loading')
+            if (useFade) {
+              imageTag.classList.add('image-lazy-active')
+              setTimeout(() => {
+                imageTag.classList.remove('image-lazy-active', 'image-lazy-init')
+              }, 300)
+            }
           })
         }
       } else {
-        image.style.backgroundImage = `url(${src})`
+        this.$utils.setStyle(imageTag, 'background-image', `url(${src})`)
       }
     }
   }
