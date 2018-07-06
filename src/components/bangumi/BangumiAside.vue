d31bfa24a81ce0da4b40ba082a8c2d8ca342ae87
falstack <icesilt@outlook.com>
2018-07-06 17:07:59 +0800

    add: become bangumi master

--- src/components/bangumi/BangumiAside.vue
+++ src/components/bangumi/BangumiAside.vue
@@ -1,19 +1,34 @@
 <style lang="scss">
   #bangumi-aside {
-    #tags {
+    >div {
       margin-bottom: 20px;
+    }
 
+    .no-one {
+      color: $color-text-normal;
+    }
+
+    #bangumi-tags {
       li {
         margin-right: 10px;
         margin-bottom: 10px;
         float: left;
       }
     }
+  }
 
-    #followers {
-      .no-one {
-        color: $color-text-normal;
-      }
+  .request-become-master-modal {
+    p {
+      margin-top: 10px;
+      margin-bottom: 10px;
+      line-height: 20px;
+    }
+    ul {
+      margin-left: 25px;
+    }
+    li {
+      list-style-type: disc;
+      line-height: 20px;
     }
   }
 </style>
@@ -22,7 +37,7 @@
   <div id="bangumi-aside">
     <div
       v-if="tags.length"
-      id="tags"
+      id="bangumi-tags"
     >
       <h2 class="sub-title">标签</h2>
       <ul>
@@ -40,7 +55,7 @@
         </li>
       </ul>
     </div>
-    <div id="followers">
+    <div id="bangumi-followers">
       <h2 class="sub-title">关注的人{{ info.count_like ? `（${info.count_like}）` : '' }}</h2>
       <ava-dialog
         v-if="info.count_like"
@@ -55,6 +70,40 @@
         class="no-one"
       >还没有人关注</span>
     </div>
+    <div id="bangumi-managers">
+      <h2 class="sub-title">版主</h2>
+      <ava-dialog
+        v-if="managers.length"
+        :users="managers"
+        :fetch="fetchMoreManagers"
+        :title="`《${info.name}》的版主们`"
+        :loading="false"
+        :no-more="true"
+      />
+      <el-button
+        round
+        size="mini"
+        type="primary"
+        @click="openRequestMasterModal = true"
+      >还没有版主，点击申请</el-button>
+      <v-dialog
+        v-model="openRequestMasterModal"
+        :title="`申请成为《${info.name}》的版主`"
+        :footer="false"
+        class="request-become-master-modal"
+      >
+        <p>亲爱的漫友你好，欢迎来到 calibur.tv！</p>
+        <p>如果你成为了版主，那就代表你成为了这里重要的一员，因此我们在甄选版主时会非常谨慎，你起码要满足一下要求：</p>
+        <ul>
+          <li>你要有足够的阅片量，是个真正的漫迷，足够强</li>
+          <li>你要有充足的精力和耐心，与 calibur 一同成长</li>
+          <li>你要有负责人的精神，不能滥用权限</li>
+          <li>你要待人和善，不能有意伤害其他漫友</li>
+        </ul>
+        <p>如果你满足以上条件，请尽快联系我们，组织需要你！</p>
+        <p>请到 <a href="/about/hello">「交流与反馈」</a>页面寻找 QQ 群加入我们！</p>
+      </v-dialog>
+    </div>
   </div>
 </template>
 
@@ -63,9 +112,8 @@
     name: 'BangumiAside',
     data () {
       return {
-        openFollowersModal: false,
         loadingFollowers: false,
-        fetchFollowersCount: 10
+        openRequestMasterModal: false
       }
     },
     computed: {
@@ -78,11 +126,16 @@
       followers () {
         return this.info.followers
       },
-      displayFollowers () {
-        return this.followers.slice(0, 7)
-      },
       noMoreFollowers () {
         return this.info.noMoreFollowers
+      },
+      managers () {
+        if (!this.info.managers.length) {
+          return []
+        }
+        return this.info.managers.map(_ => {
+          return _.user
+        });
       }
     },
     methods: {
@@ -95,7 +148,7 @@
           await this.$store.dispatch('bangumi/getFollowers', {
             ctx: this,
             bangumiId: this.info.id,
-            take: this.fetchFollowersCount
+            take: 10
           })
         } catch (e) {
           this.$toast.error(e)
@@ -103,6 +156,9 @@
           this.loadingFollowers = false
         }
       },
+      fetchMoreManagers () {
+        // 现在最多5个，没有分页
+      }
     }
   }
 </script>
