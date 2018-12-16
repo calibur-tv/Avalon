5f07be49c177cbf913642f0471449f8186e16129
falstack <icesilt@outlook.com>
2018-12-16 23:24:08 +0800

    add: cartoon sort

--- pages/bangumi/_id/cartoon.vue
+++ pages/bangumi/_id/cartoon.vue
@@ -1,10 +1,44 @@
+<style lang="scss">
+#bangumi-cartoon-flow {
+  .header {
+    margin-bottom: 15px;
+    text-align: right;
+
+    .total {
+      float: left;
+      line-height: 28px;
+    }
+
+    .el-select {
+      margin-right: 8px;
+    }
+  }
+}
+</style>
+
 <template>
   <div id="bangumi-cartoon-flow">
+    <div class="header">
+      <strong class="total">共 {{ source.total }} 集</strong>
+      <el-select
+        v-model="selectedSort"
+        :loading="source.loading"
+        size="mini"
+        @change="handleSortSwitch"
+      >
+        <el-option
+          v-for="item in order"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
+    </div>
     <flow-list
       :id="id"
+      :sort="sort"
       func="getBangumiCartoon"
       type="page"
-      sort="asc"
     >
       <ul slot-scope="{ flow }">
         <cartoon-flow-item
@@ -38,6 +72,44 @@ export default {
     id: {
       required: true,
       type: String
+    }
+  },
+  data() {
+    return {
+      selectedSort: 'asc',
+      sort: 'asc'
+    }
+  },
+  computed: {
+    source() {
+      return this.$store.getters['flow/getFlow'](
+        'getBangumiCartoon',
+        this.sort,
+        this.id
+      )
+    },
+    order() {
+      return [
+        {
+          label: '由大到小排序',
+          value: 'desc'
+        },
+        {
+          label: '由小到大排序',
+          value: 'asc'
+        }
+      ]
+    }
+  },
+  methods: {
+    async handleSortSwitch(item) {
+      await this.$store.dispatch('flow/initData', {
+        id: this.id,
+        func: 'getBangumiCartoon',
+        type: 'page',
+        sort: item
+      })
+      this.sort = item
     }
   }
 }
