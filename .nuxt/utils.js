f84a42fa2905efd0c2e217945196b4e2e56ec309
falstack <icesilt@outlook.com>
2019-01-07 22:15:24 +0800

    update: video page

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -129,7 +129,7 @@ export async function setContext(app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {"API_URL":"http://localhost/","API_URL_BROWSER":"https://api.calibur.tv/","SENTRY_URL":"https://5c1d2b169b09423abb6a74227a64c3c4@sentry.io/1352871","RELEASE":"2019-1-7 13:47:16"}
+      env: {"API_URL":"http://localhost/","API_URL_BROWSER":"https://api.calibur.tv/","SENTRY_URL":"https://5c1d2b169b09423abb6a74227a64c3c4@sentry.io/1352871","RELEASE":"2019-1-7 22:12:24"}
     }
     // Only set once
     if (context.req) app.context.req = context.req
