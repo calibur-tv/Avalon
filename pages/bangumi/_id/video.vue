7778e19f14c19f68b4008533fcd5c9dc63be3a81
falstack <icesilt@outlook.com>
2019-02-15 11:58:24 +0800

    change: video upload role

--- pages/bangumi/_id/video.vue
+++ pages/bangumi/_id/video.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="bangumi-video-flow">
     <el-alert
-      v-if="bangumi.is_leader"
+      v-if="bangumi.is_leader || bangumi.is_master"
       type="info"
       title=""
       style="margin-bottom:15px"
     >
-      版主有权限上传百度云资源的视频，请勿引用包含广告的资源，违者抹杀（笑&nbsp;
+      版主和代行者有权限上传百度云资源的视频，请勿引用包含广告的资源，违者抹杀（笑&nbsp;
       <el-button
         type="primary"
         size="mini"
