489aa62178d7de77317c88a01e82e04ce5fd1c5c
falstack <icesilt@outlook.com>
2019-02-20 09:00:57 +0800

    fixed: typo

--- components/idol/ChangeIdolProfile.vue
+++ components/idol/ChangeIdolProfile.vue
@@ -35,6 +35,11 @@
       v-model="qq_group"
       placeholder="填写QQ群号码"
     />
+    <div class="tips">修改经纪人</div>
+    <el-input
+      v-model="manager_id"
+      placeholder="填写要任命的经纪人的邀请码"
+    />
     <el-button
       :loading="submitting"
       type="primary"
@@ -58,6 +63,7 @@ export default {
     return {
       qq_group: this.idol.qq_group,
       lover_words: this.idol.lover_words,
+      manager_id: this.idol.manager ? this.idol.manager.id : '',
       submitting: false
     }
   },
@@ -79,6 +85,9 @@ export default {
         this.$toast.error('QQ群号必须是数字')
         return
       }
+      if (this.manager_id && !/^\d+$/.test(this.manager_id)) {
+        this.$toast.error('经纪人的id是数字')
+      }
       if (this.qq_group && !this.idol.qq_group) {
         this.$confirm('QQ群号设置之后不能再修改，确认要设置吗?', '提示', {
           confirmButtonText: '确定',
@@ -99,7 +108,8 @@ export default {
         await changeCartoonRoleProfile(this, {
           idol_id: this.idol.id,
           qq_group: this.qq_group,
-          lover_words: words
+          manager_id: this.manager_id,
+          lover_words: this.lover_words.trim()
         })
         this.$toast.success('修改成功')
         setTimeout(() => {
