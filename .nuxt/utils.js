a244f183a316225c602c9baf5890a738d2a9013f
falstack <icesilt@outlook.com>
2019-02-26 13:06:44 +0800

    rollback

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -129,7 +129,7 @@ export async function setContext(app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {"API_URL":"http://localhost/","API_URL_BROWSER":"https://api.calibur.tv/","SENTRY_URL":"https://5c1d2b169b09423abb6a74227a64c3c4@sentry.io/1352871","RELEASE":"2019-2-24-15-59-18"}
+      env: {"API_URL":"http://localhost/","API_URL_BROWSER":"https://api.calibur.tv/","SENTRY_URL":"https://5c1d2b169b09423abb6a74227a64c3c4@sentry.io/1352871","RELEASE":"2019-2-26-13-03-54"}
     }
     // Only set once
     if (context.req) app.context.req = context.req
