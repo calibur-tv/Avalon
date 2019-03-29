b5b87af255475522488b05e22cb634bcce8d076a
falstack <icesilt@outlook.com>
2019-03-29 08:56:12 +0800

    update: vue-flow-render

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -135,7 +135,7 @@ export async function setContext(app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {"API_URL":"http://localhost/","API_URL_BROWSER":"https://api.calibur.tv/","SENTRY_URL":"https://5c1d2b169b09423abb6a74227a64c3c4@sentry.io/1352871","RELEASE":"2019-3-28-20-39-44"}
+      env: {"API_URL":"http://localhost/","API_URL_BROWSER":"https://api.calibur.tv/","SENTRY_URL":"https://5c1d2b169b09423abb6a74227a64c3c4@sentry.io/1352871","RELEASE":"2019-3-29-08-54-16"}
     }
     // Only set once
     if (context.req) app.context.req = context.req
